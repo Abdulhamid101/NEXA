16596fbcf88b81b8f1ec80ffd821d0ab8d426275
kafidipe Abdulhamid <kafidipeabdulhamid@gmail.com>
2025-09-10 11:13:31 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Header from "./component/Header/Header.jsx";   // <- ensure folder is "component"
+import Header from "./component/Header/Header.jsx";
 import Footer from "./component/Footer/Footer.jsx";
-import styles from "./app.module.css";
+import styles from "./App.module.css";
 import { WalletModalProvider } from "./context/WalletModalContext.jsx";
 
 export default function App() {
